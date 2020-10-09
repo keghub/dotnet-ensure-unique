604db7ecacb1200e78f96c528b092940b01a74a7
Renato Golia <renato.golia@educations.com>
2020-10-09 15:59:32 +0200

    Added helper programs
    These programs are meant to be executed through the tool
    - OkConsole => executes successfully
    - ErrorConsole => unhandled user exception
    - StackOverflowConsole => fatal runtime exception

---
+++ helpers/ErrorConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErrorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new Exception("Something went wrong");
+        }
+    }
+}
